fb8483ff0e0b898482e59088feb725a34985ba94
chika-bac <frefg.cmw@gmail.com>
2025-07-23 01:02:47 +0900

    Controllerのテスト（registerStudentとupdateStudentと例外API）を追加

--- src/test/java/student/management/StudentManagement/controller/StudentControllerTest.java
+++ src/test/java/student/management/StudentManagement/controller/StudentControllerTest.java
@@ -1,8 +1,11 @@
 package student.management.StudentManagement.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import jakarta.validation.ConstraintViolation;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import student.management.StudentManagement.data.Student;
@@ -30,11 +34,82 @@ class StudentControllerTest {
   private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
   @Test
-  void 受講生詳細の一覧検索ができて空のリストが返ってくること() throws Exception {
+  void 受講生詳細の一覧検索ができること() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("/studentList"))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andExpect(content().json("[]"));
 
     verify(service, times(1)).getAllStudents();
+  }
+
+  @Test
+  void 受講生詳細の検索ができて空で返ってくること() throws Exception {
+    String id = "1";
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/student/{id}", id))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).searchStudent(id);
+  }
+
+  @Test
+  void 受講生詳細の新規登録ができて空で返ってくること() throws Exception {
+    mockMvc.perform(post("/registerStudent")
+            .contentType(MediaType.APPLICATION_JSON).content(
+                """
+                    {
+                        "student": {
+                            "name": "山田花子",
+                            "kanaName": "ヤマダハナコ",
+                            "nickname": "はなこさん",
+                            "email": "test@example.com",
+                            "city": "長崎県佐世保市",
+                            "age": 20,
+                            "gender": "女性",
+                            "remark": "テスト"
+                        },
+                        "studentCourseList": [
+                          {
+                            "courseName": "Javaフルコース"
+                          }
+                        ]
+                    }
+                    """
+            ))
+        .andExpect(status().isCreated());
+
+    verify(service, times(1)).registerStudent(any());
+  }
+
+  @Test
+  void 受講生詳細の更新ができて空で返ってくること() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.put("/updateStudent")
+            .contentType(MediaType.APPLICATION_JSON).content(
+                """
+                    {
+                        "student": {
+                            "id": "1",
+                            "name": "山田花子",
+                            "kanaName": "ヤマダハナコ",
+                            "nickname": "はなこさん",
+                            "email": "test@example.com",
+                            "city": "東京都新宿区",
+                            "age": 20,
+                            "gender": "女性",
+                            "remark": "テスト"
+                        },
+                        "studentCourseList": [
+                          {
+                            "id": "1",
+                            "courseName": "Javaフルコース"
+                          }
+                        ]
+                    }
+                    """
+            ))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).updateStudent(any());
   }
 
   @Test
@@ -73,5 +148,12 @@ class StudentControllerTest {
     assertThat(violations)
         .extracting("message")
         .containsOnly("有効なメールアドレスを入力してください。");
+  }
+
+  @Test
+  void 受講生詳細の例外APIが実行できてステータスが400で返ってくること() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/studentException"))
+        .andExpect(status().is4xxClientError())
+        .andExpect(content().string("現在このAPIは利用できません。新しいURLは「students」です。"));
   }
 }
